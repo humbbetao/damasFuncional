799d03fe724fc48fedf7a3c9872b4df42d32821c
Ana <annafrozza@gmail.com>
2016-05-22 18:11:55 -0300

    printar matriz

--- damas.ml
+++ damas.ml
@@ -7,22 +7,32 @@ let rec criarMatrizInicial matriz lista =
 let bemvindo  = print_string "Bem vindo ao jogo de damas";;
 
 
+let rec printaMatriz matriz =
+  match matriz with
+    | []-> print_string "|"
+    | hd::ht -> print_string hd ; printaMatriz ht
+;;
+
+
 (*chamada de execucao*)
 criarMatrizInicial  [] [["B";"R";"B";"R";"B";"R";"B";"R";"B";];["R";"B";"R";"B";"R";"B";"R";"B";"R";]; ["B";"R";"B";"R";"B";"R";"B";"R";"B";];["P";"B";"P";"B";"P";"B";"P";"B";"P";];["B";"P";"B";"P";"B";"P";"B";"P";"B";];["C";"B";"C";"B";"C";"B";"C";"B";"C";];["B";"C";"B";"C";"B";"C";"B";"C";"B";];["C";"B";"C";"B";"C";"B";"C";"B";"C";];]
 ;;
 
 
 let rec escolhaDaPosicao =
-  let () = print_string "Digite a linha: " in
+  let () = print_string " Digite a linha: " in
   let i = read_int ()  in
   let ()  = print_string "Digite a coluna: " in
   let j = read_int()  in
 
+	print_int i; print_int j; print_string "\n";;
 
-    print_int i;;
+(*escolhaDaPosicao*)
 
-escolhaDaPosicao
 
+printaMatriz ["B";"R";"B";"R";"B";"R";"B";"R";"B";]
+;; [["B";"R";"B";"R";"B";"R";"B";"R";"B";];["R";"B";"R";"B";"R";"B";"R";"B";"R";]; ["B";"R";"B";"R";"B";"R";"B";"R";"B";];["P";"B";"P";"B";"P";"B";"P";"B";"P";];["B";"P";"B";"P";"B";"P";"B";"P";"B";];["C";"B";"C";"B";"C";"B";"C";"B";"C";];["B";"C";"B";"C";"B";"C";"B";"C";"B";];["C";"B";"C";"B";"C";"B";"C";"B";"C";];]
+;;
 
 
 (*
