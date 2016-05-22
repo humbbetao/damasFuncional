fd701a25cd40dc81f08e6f7be60de515f9279aac
humbbetao <humberto_voleibol@hotmail.com>
2016-05-22 13:58:04 -0300

    Add

--- damas.ml
+++ damas.ml
@@ -1,1 +1,58 @@
-print_string "Bem vindo ao jogos de damas";;
+(*
+;;
+
+let menos posicao  = posicao - 1
+;;
+
+let rec inserir lista e =
+match lista with
+| [] ->[e] 
+| hd::ht->hd :: (inserir ht e)
+;;*)
+
+(*crio a matriz Inicial *)
+let damas =
+  print_string "Bem vindo a damas feito em ocaml" ;
+  (*encadeamento de funcoes*)
+  let rec criarMatrizInicial matriz lista = 
+    match matriz with
+      | []-> lista
+      | hd::ht -> hd :: criarMatrizInicial ht lista;
+  in
+  let lerDoTeclado matriz  = 
+    print_string "Digite a posicao que voce quer comecar";
+    print_string "Voce e as pecas brancas";
+    let i = read_int ()
+              print_int i 
+;;
+
+
+
+damas;;
+
+(* tento mover uma peca com a cor e a peca 
+   let rec moverPeca matriz cor linha coluna = 
+
+
+   ;;
+*)
+
+(*
+let
+val BRANCO = "B"
+val PRETO = "P"
+val VERMELHO = "R"
+val CINZA = "C"
+val QUEEN = "Q"
+val KING = "K"
+in
+end;*)
+
+(* B = branco, P = preto, R = vermelha; C = Cinza *)
+
+
+(* daqui pra baixo eh so chamada de execucao*)
+(*chamada de execucao*)(*
+criarMatrizInicial  [] [["B";"R";"B";"R";"B";"R";"B";"R";"B";];["R";"B";"R";"B";"R";"B";"R";"B";"R";]; ["B";"R";"B";"R";"B";"R";"B";"R";"B";];["P";"B";"P";"B";"P";"B";"P";"B";"P";];["B";"P";"B";"P";"B";"P";"B";"P";"B";];["C";"B";"C";"B";"C";"B";"C";"B";"C";];["B";"C";"B";"C";"B";"C";"B";"C";"B";];["C";"B";"C";"B";"C";"B";"C";"B";"C";];]
+;;*)
+
