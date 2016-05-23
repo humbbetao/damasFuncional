b2a6b83a40823bdb5770a8845a5310fb194247bc
Ana <annafrozza@gmail.com>
2016-05-22 21:07:49 -0300

    MoverPeca

--- damas.ml
+++ damas.ml
@@ -20,6 +20,45 @@ let rec criarMatrizInicial matriz lista =
     | hd::ht -> hd :: criarMatrizInicial ht lista
 ;;
 
+let rec moverPeca matriz peca i j x y = 
+	match matriz with
+		| [] -> []
+		| hd::ht -> if x > 0 then (moverPeca ht i j (x-1) y ) else (moverY hd i j x (y-1)) 
+		and moverY hd i j x y  =
+			match hd with
+				| [] -> print_string ""
+				| hd::hy::ht -> if y >0 then moverY ht i j x (y-1) else hd ::"R" ::ht; printaMatriz matriz 0
+		;;
+
+let rec mover matriz i j x y = 
+	match matriz with
+		| [] -> []
+		| hd::ht -> if i >0 then mover ht (i-1) j x y else moverJ hd i (j-1) x y  
+		and moverJ hd i j x y =
+			match hd with
+				| [] -> print_string ""
+				| hd::hj::ht -> if j >0 then moverJ ht i (j-1) x y else hd::"P"::ht;  moverPeca matriz hd i j x y
+		;;
+		
+		(*
+			and	escreverLista lista i = 
+			match lista with
+				| []-> []
+				| hd::ht-> if matriz[i][j] = "R" then (matriz[x][y] = "R"; matriz[i][j] = "P";) 
+							else print_string hd; print_string" | "; (printarLista ht (i+1)) ;;						
+
+*)
+let rec printaMatrizInicial matriz j = 
+	match matriz with
+		| [] -> []
+		| hd::ht -> print_string "\n"; print_int j; print_string " - "; (printarLista hd (j+1) ); print_string "\n"; (printaMatrizInicial ht  (j+1) )
+		and	printarLista lista i = 
+			match lista with
+				| []->  print_string ""
+				| hd::ht-> if i =0 then (print_string" | "; print_int i;  print_string" - "; print_string hd;)
+							else print_string hd; print_string" | "; (printarLista ht (i+1)) ;;						
+
+
 let rec printaMatriz matriz j = 
 	match matriz with
 		| [] -> []
@@ -35,16 +74,21 @@ print_string "Bem vindo ao jogo de damas";;
 print_string "\nJogo de Damas Atual";;
 print_string "\n    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8";;
 print_string "\n    .   .   .   .   .   .   .   .";;
-printaMatriz (criarMatrizInicial [] [["B";"R";"B";"R";"B";"R";"B";"R";];["R";"B";"R";"B";"R";"B";"R";"B";]; ["B";"R";"B";"R";"B";"R";"B";"R";];["P";"B";"P";"B";"P";"B";"P";"B";];["B";"P";"B";"P";"B";"P";"B";"P";];["C";"B";"C";"B";"C";"B";"C";"B";];["B";"C";"B";"C";"B";"C";"B";"C";];["C";"B";"C";"B";"C";"B";"C";"B";];]) 1
+printaMatrizInicial (criarMatrizInicial [] [["B";"R";"B";"R";"B";"R";"B";"R";];["R";"B";"R";"B";"R";"B";"R";"B";]; ["B";"R";"B";"R";"B";"R";"B";"R";];["P";"B";"P";"B";"P";"B";"P";"B";];["B";"P";"B";"P";"B";"P";"B";"P";];["C";"B";"C";"B";"C";"B";"C";"B";];["B";"C";"B";"C";"B";"C";"B";"C";];["C";"B";"C";"B";"C";"B";"C";"B";];]) 1
 ;;
 print_string "Sua Jogada\n ";;
 
+
 let rec escolhaDaPosicao =
-  let () = print_string "Digite a linha: " in
+  let () = print_string "Digite a linha em que você esta: " in
   let i = read_int ()  in
-  let ()  = print_string "Digite a coluna: " in
+  let ()  = print_string "Digite a coluna em que você esta: " in
   let j = read_int()  in  
-    	print_int i; print_int j; print_string "\n"
+  let () = print_string "Digite a linha para a qual deseja se mover: " in
+  let x = read_int() in
+  let () = print_string "Digite a coluna para a qual deseja se mover: " in
+  let y = read_int() in
+		print_int x; print_int y; print_string "\n"
 ;;
 	
 	
