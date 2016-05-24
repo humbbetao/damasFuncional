7e420d49e19af24e97bf218b8927240c2b8505b4
humbbetao <humberto_voleibol@hotmail.com>
2016-05-24 16:48:59 -0300

    editei algumas funcoes de inicio e turno

--- damas.ml
+++ damas.ml
@@ -26,8 +26,15 @@ let rec printarLista lista i =
 let rec printaMatrizInicial matriz j = 
 	match matriz with
 		| [] -> []
-		| hd::ht -> print_string "\n"; print_int j; print_string " - "; (printarLista hd (j+1) ); print_string "\n"; (printaMatrizInicial ht  (j+1) );;
-	
+		| hd::ht ->  print_string "\n"; print_int j; print_string " - "; (printarLista hd (j+1) ); print_string "\n"; (printaMatrizInicial ht  (j+1) ) ;;
+(*comentario teste
+let rec printaMatrizInicial matriz j = 
+	match matriz with
+		| [] -> []
+		| hd::ht -> if j=1  then print_string "\nJogo de Damas Atual" ;	print_string "\n    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8";	print_string "\n    .   .   .   .   .   .   .   .";	print_string "\n"; print_int j; print_string " - "; (printarLista hd (j+1) );print_string "\n";	(printaMatrizInicial ht (j+1) )
+					else print_string "\n"; print_int j; print_string " - "; (printarLista hd (j+1) ); print_string "\n"; (printaMatrizInicial ht  (j+1) ) ;;
+	*)
+					
 
 let rec printaMatriz matriz j = 
 	match matriz with
@@ -46,35 +53,38 @@ let rec criarMatrizInicial matriz lista =
     | hd::ht -> hd :: criarMatrizInicial ht lista
 ;;
 
+let rec moverY hd peca i j x y  =
+	match hd with
+		| [] -> []
+		| hd::ht -> if y >0 then (moverY ht peca i j x (y-1)) else peca::ht;
+		;;
+
 let rec moverPeca matriz peca i j x y = 
 	match matriz with
 		| [] -> []
-		| hd::ht -> if x > 0 then (moverPeca ht peca i j (x-1) y ) else (moverY hd peca i j x (y-1)) 
-		and moverY hd peca i j x y  =
-			match hd with
-				| [] -> []
-				| hd::ht -> if y >0 then (moverY ht peca i j x (y-1)) else peca::ht;
-		;;
+		| hd::ht -> if x > 0 then (moverPeca ht peca i j (x-1) y ) else (moverY hd peca i j x (y-1)) ;;
 
+		
+let rec moverJ hd i j x y =
+	match hd with
+		| [] -> []
+		| hd::ht -> if j >0 then (moverJ ht i (j-1) x y) else "P"::ht;;
+				
 let rec mover matriz i j x y = 
 	match matriz with
 		| [] -> []
-		| hd::ht -> if i >0 then mover ht (i-1) j x y else moverJ hd i (j-1) x y  
-		and moverJ hd i j x y =
-			match hd with
-				| [] -> []
-				| hd::ht -> if j >0 then (moverJ ht i (j-1) x y) else "P"::ht;
-				
-;;
-print_string "Bem vindo ao jogo de damas";;
-print_string "\nJogo de Damas Atual";;
-print_string "\n    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8";;
-print_string "\n    .   .   .   .   .   .   .   .";;
-let m = printaMatrizInicial (criarMatrizInicial [] [["B";"R";"B";"R";"B";"R";"B";"R";];["R";"B";"R";"B";"R";"B";"R";"B";]; ["B";"R";"B";"R";"B";"R";"B";"R";];["P";"B";"P";"B";"P";"B";"P";"B";];["B";"P";"B";"P";"B";"P";"B";"P";];["C";"B";"C";"B";"C";"B";"C";"B";];["B";"C";"B";"C";"B";"C";"B";"C";];["C";"B";"C";"B";"C";"B";"C";"B";];]) 1
-;;
+		| hd::ht -> if i >0 then mover ht (i-1) j x y else moverJ hd i (j-1) x y  ;;
 
-print_string "Sua Jogada\n ";;
+		
 
+		
+let bemVindo = print_string "Bem vindo ao jogo de damas";;
+
+let inicioDeTurno = print_string "\nJogo de Damas Atual"; print_string "\n    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8"; print_string "\n    .   .   .   .   .   .   .   .";;
+
+let turno jogador  =  if  (jogador mod 2 ) = 0 then print_string "Turno do Jogador "  else print_string "Turno do Computador";;
+
+let m = printaMatrizInicial (criarMatrizInicial [] [["B";"R";"B";"R";"B";"R";"B";"R";];["R";"B";"R";"B";"R";"B";"R";"B";]; ["B";"R";"B";"R";"B";"R";"B";"R";];["P";"B";"P";"B";"P";"B";"P";"B";];["B";"P";"B";"P";"B";"P";"B";"P";];["C";"B";"C";"B";"C";"B";"C";"B";];["B";"C";"B";"C";"B";"C";"B";"C";];["C";"B";"C";"B";"C";"B";"C";"B";];]) 1;;
 
 let rec escolhaDaPosicao =
   let () = print_string "Digite a linha em que voce esta: " in
@@ -85,8 +95,12 @@ let rec escolhaDaPosicao =
   let x = read_int() in
   let () = print_string "Digite a coluna para a qual deseja se mover: " in
   let y = read_int() in
-		print_string "\n";  mover m i j x y;
+		print_string "\n"
 ;;
 
-printaMatrizInicial m 0;;		
-	
+		
+		
+bemVindo;;
+turno 0;;
+printaMatrizInicial m 1;;	
+escolhaDaPosicao;;
