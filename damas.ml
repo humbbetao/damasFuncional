ae3765a2907d1be5e010e7fe0a530f8360e1398e
humbbetao <humberto_voleibol@hotmail.com>
2016-05-25 01:52:17 -0300

    Ta certo sof alta turnar

--- damas.ml
+++ damas.ml
@@ -53,6 +53,41 @@ let rec criarMatrizInicial matriz lista =
     | hd::ht -> hd :: criarMatrizInicial ht lista
 ;;
 
+let rec substituirPeca matriz peca x y = 
+	match matriz with
+		| []->[]
+		| hd::ht ->hd::ht;;
+(*		
+let rec verificarPeca cabeca peca lista ht =
+	if peca= "R" then lista::"P"::ht
+	else lista::"P"::ht ;;
+*)
+let rec verificarPeca peca lista ht =
+	if peca= "R" then lista::"P"::ht
+	else lista::"P"::ht ;;
+	
+	
+let rec acharPecaColuna linha lista peca i j =
+	match linha with	
+		| [] -> []
+		| hd::ht -> if j=0 then "P"::ht else acharPecaColuna ht (lista) peca i (j-1);;
+		
+let rec acharPeca matriz peca i j = 
+	match matriz with 
+		| [] -> []		
+		| hd::ht-> if i=0 then acharPecaColuna hd [] peca i j else acharPeca ht peca (i-1) j ;; 
+
+(*let rec moverPeca matriz i j x y peca = 
+	match matriz with
+		| [] -> []
+		| hd::ht -> if (i>0 j>0) then (acharPeca matriz peca i j)
+				else (substituirPeca matriz peca x y);;
+	*)	
+
+		
+		
+		
+(*		
 let rec moverY hd peca x y  =
 	match hd with
 		| [] -> []
@@ -65,16 +100,16 @@ let rec moverPeca matriz peca x y =
 		| hd::ht -> if x > 0 then (moverPeca ht peca (x-1) y ) else (moverY hd peca x (y-1)) ;;
 
 		
-let rec moverJ hd lista i j  =
+let rec andarEmColuna hd lista i j  =
 	match hd with
 		| [] -> []
 		| hd::ht -> if j >0 then (moverJ ht hd::lista i (j-1)) else lista::"P"::ht ;;
 				
-let rec mover matriz lista i j  = 
+let rec andarEmLinha matriz lista i j  = 
 	match matriz with
 		| [] -> []
-		| hd::ht -> if i > 0 then mover ht hd::lista (i-1) j else moverJ hd [] i (j-1)  ;;
-
+		| hd::ht -> if i > 0 then andarEmLinha ht hd::lista (i-1) j else andarEmLinha hd [] i (j-1)  ;;
+*)
 
 
 		
@@ -96,7 +131,7 @@ let digitarALinhaParaQueVai = print_string "Digite a linha para a qual deseja se
 let digitarAColunaParaQueVai = print_string "Digite a coluna para a qual deseja se mover: ";
 *)
 
-let rec escolhaDaPosicao =
+let escolhaDaPosicao m1 m =
 	let () = print_string "Digite a linha em que voce esta: " in 
 	let i = read_int () in 
 	let () = print_string "Digite a coluna em que voca esta: " in
@@ -104,7 +139,26 @@ let rec escolhaDaPosicao =
 	let () = print_string "Digite a linha para a qual deseja se mover: " in
 	let x = read_int() in	
 	let () = print_string "Digite a coluna para a qual deseja se mover: " in
-	let y = read_int() in  print_string "\n"; moverPeca m i j x y ;;
+	let y = read_int() in  acharPeca m i j;;	
+	
+	(*
+let jogada turno =
+	printaMatriz m 1
+
+let turnar matriz  turno = if (0 mod 2) =0 then jogada turno+1 matriz else  jogadaComputador turno+1 matriz;;
+
+	
+	
+let damas = 	
+	let matrizOriginal = criarMatrizInicial [] [["B";"R";"B";"R";"B";"R";"B";"R";];["R";"B";"R";"B";"R";"B";"R";"B";]; ["B";"R";"B";"R";"B";"R";"B";"R";];["P";"B";"P";"B";"P";"B";"P";"B";];["B";"P";"B";"P";"B";"P";"B";"P";];["C";"B";"C";"B";"C";"B";"C";"B";];["B";"C";"B";"C";"B";"C";"B";"C";];["C";"B";"C";"B";"C";"B";"C";"B";];]
+		let turnar matrizOriginal 0 ;;
+	*)
+let damas = 	
+	let matrizOriginal = criarMatrizInicial [] [["B";"R";"B";"R";"B";"R";"B";"R";];["R";"B";"R";"B";"R";"B";"R";"B";]; ["B";"R";"B";"R";"B";"R";"B";"R";];["P";"B";"P";"B";"P";"B";"P";"B";];["B";"P";"B";"P";"B";"P";"B";"P";];["C";"B";"C";"B";"C";"B";"C";"B";];["B";"C";"B";"C";"B";"C";"B";"C";];["C";"B";"C";"B";"C";"B";"C";"B";];]
+	in escolhaDaPosicao (printaMatrizInicial matrizOriginal 1 ) matrizOriginal ;;
+	
+
+	(* x y "P";;*)
 	(*
   let i = read_int ()  in
   let digitarAColunaEmQueEsta  = print_string "Digite a coluna em que voca esta: " in
@@ -117,8 +171,10 @@ let rec escolhaDaPosicao =
 
 		
 		
-bemVindo;;
-printaMatrizInicial m 1;;	
+(*bemVindo;;*)
+(*printaMatrizInicial m 1;;	
 escolhaDaPosicao;;
 turno 0;;
-
+printaMatrizInicial m 1;;
+*)
+damas;;
