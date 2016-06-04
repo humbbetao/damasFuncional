ca673aaf766b5d06eccc232f56796a7edb1bd1d7
humbbetao <humberto_voleibol@hotmail.com>
2016-06-03 21:04:23 -0300

    Modificacao no iniico do jogo e correcao de passagem de parametros

--- damas.ml
+++ damas.ml
@@ -9,7 +9,7 @@ DAMA PRETA = "Q"
 				
 let rec inserirNaUltimaPosicao lista elemento = 
 	match lista with	
-		| []-> elemento
+		| []-> [elemento]
 		| hd::ht -> inserirNaUltimaPosicao ht elemento
 ;;
 
@@ -21,17 +21,15 @@ let rec substituirPecaEmColuna lista listaAuxiliar j peca  =
 ;;					
 let rec substituirPeca matriz matrizAuxiliar i j peca =
 	match matriz with
-		| []->[]
-		| hd::ht -> if i = 0 then  inserirNaUltimaPosicao matrizAuxiliar (substituirPecaEmColuna hd [] j peca::ht ) 
+		| []-> []
+		| hd::ht -> if i = 0 then  (inserirNaUltimaPosicao(inserirNaUltimaPosicao matrizAuxiliar (substituirPecaEmColuna hd [] j peca)) ht)
 					else substituirPeca ht (inserirNaUltimaPosicao matrizAuxiliar hd) (i-1) j peca
 ;;
 
-
-
-let moverPecaDeIJParaXY matriz i j x y = substituirPeca (substituirPeca matriz [] i j ["1"]) [] x y ["B"]
+let moverPecaDeIJParaXY matriz i j x y = substituirPeca (substituirPeca matriz [] i j "1" ) [] x y "B" 
 ;;
 
-let comerPecaDeIJParaXY  matriz i j x y k l = substituirPeca (substituirPeca (substituirPeca matriz [] k l ["1"]) [] i j ["1"]) [] x y ["B"]
+let comerPecaDeIJParaXY  matriz i j x y k l = substituirPeca (substituirPeca (substituirPeca matriz [] k l "1") [] i j "1") [] x y "B"
 ;;
 
 
@@ -52,23 +50,47 @@ let pecaCapturada matriz i j x y =
 
 
 
+let moverPeca matriz i j x y =  moverPecaDeIJParaXY matriz y x j i 
+;;
+(*
 let moverPeca matriz i j x y =   
 	if i = (x-1) then moverPecaDeIJParaXY matriz y x j i 
 	else if i = (x+1) then moverPecaDeIJParaXY matriz y x j i 
 	else pecaCapturada matriz y x j i
-;;
+;;*)
 
 let turnoDoJogador matriz = moverPeca matriz (escolhaDaPosicao 3) (escolhaDaPosicao 2) (escolhaDaPosicao 1) (escolhaDaPosicao 0)(*o primeiro eh na linha e depois coluna da linha que ele esta*)
 ;;
+	
+let turnar matriz numeroDeTurno =  turnoDoJogador matriz
+;;	
 
-let turnoDoJogador matriz = moverPeca matriz (escolhaDaPosicao 3) (escolhaDaPosicao 2) (escolhaDaPosicao 1) (escolhaDaPosicao 0) 
+let rec printarMatriz matriz turno j matrizNova =
+	match matriz with
+		| [] -> []
+		| hd::ht -> if j =1 then	( print_string "\n\nJogo Atual :\n\n";
+									print_string"    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |";
+									print_string "\n    .   .   .   .   .   .   .   . ";
+									print_string "\n";
+									print_int j;
+									print_string " - ";	
+									(printarLista hd 1 j matrizNova turno); 
+									print_string "\n";
+									(printarMatriz ht turno (j+1) (inserirNaUltimaPosicao matrizNova hd)))
+					else 			(print_string "\n";
+									print_int j;
+									print_string " - ";	
+									(printarLista hd 1 j matrizNova turno); 
+									print_string "\n";
+									(printarMatriz ht turno (j+1) (inserirNaUltimaPosicao matrizNova hd)))
+		
+		and	printarLista lista i j matriz turno  = 
+			match lista with
+				| []-> if ((i=8) && (j=8)) then (printarMatriz (turnar matriz turno) (turno+1) 0 [])
+				| hd::ht -> print_string hd; print_string" | "; (printarLista ht (i+1) j matriz turno) 
 ;;
 
-	
-let turnar matriz numeroDeTurno =
-	 turnoDoJogador matriz
-	;;	
-
+(*
 let rec printarMatriz matriz turno j matrizNova =
 	match matriz with
 		| [] -> []
@@ -80,11 +102,18 @@ let rec printarMatriz matriz turno j matrizNova =
 					(printarMatriz ht turno (j+1) (inserirNaUltimaPosicao matrizNova hd));
 		and	printarLista lista i j matriz turno  = 
 			match lista with
-				| []-> 	print_string "\n"; printarMatriz (turnar matriz turno) (turno+1) 0 []
-				| hd::ht -> print_string hd; print_string" | "; printarLista ht (i+1) j [] turno
+				| []-> if ((i=8) && (j=8)) then (printarMatriz (turnar matriz turno) (turno+1) 0 [])
+				| hd::ht -> print_string hd; print_string" | "; (printarLista ht (i+1) j matriz turno) 
 ;;
 
-printarMatriz [["0";"B";"0";"B";"0";"B";"0";"B";];
+
+*)
+print_string "Bem vindo ao Jogo de Damas \n";;
+print_string "Legenda:\n ";;
+print_string "Peca branca:  B\n Peca preta: P \n Peca Dama branca: Q\n Peca Dama Preta K \n Casa Do tabuleiro Branca: 0\n Casa Do Tabuleiro: 1";;
+
+
+printarMatriz  [["0";"B";"0";"B";"0";"B";"0";"B";];
 				["B";"0";"B";"0";"B";"0";"B";"0";];
 				["0";"B";"0";"B";"0";"B";"0";"B";];
 				["1";"0";"1";"0";"1";"0";"1";"0";];
@@ -92,5 +121,17 @@ printarMatriz [["0";"B";"0";"B";"0";"B";"0";"B";];
 				["P";"0";"P";"0";"P";"0";"P";"0";];
 				["0";"P";"0";"P";"0";"P";"0";"P";];
 				["P";"0";"P";"0";"P";"0";"P";"0";]]
-				0 1 [[]]					
+				0 1 				
 ;;
+
+(*
+printarMatriz  [[["0"];["B"];["0"];["B"];["0"];["B"];["0"];["B"];];
+				[["B"];["0"];["B"];["0"];["B"];["0"];["B"];["0"];];
+				[["0"];["B"];["0"];["B"];["0"];["B"];["0"];["B"];];
+				[["1"];["0"];["1"];["0"];["1"];["0"];["1"];["0"];];
+				[["0"];["1"];["0"];["1"];["0"];["1"];["0"];["1"];];
+				[["P"];["0"];["P"];["0"];["P"];["0"];["P"];["0"];];
+				[["0"];["P"];["0"];["P"];["0"];["P"];["0"];["P"];];
+				[["P"];["0"];["P"];["0"];["P"];["0"];["P"];["0"];]]
+				0 1 [[ ]]					
+;;*)
