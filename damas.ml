849c47e24a74ccc93f036e461ca7b688a2c25205
humbbetao <humberto_voleibol@hotmail.com>
2016-06-05 21:11:49 -0300

    turno do computador so duplica a matriz

--- damas.ml
+++ damas.ml
@@ -28,24 +28,26 @@ let rec appendLista2 lista elemento =
 let rec substituirPecaEmColuna lista j peca  = 
 	match lista with
 		| [] ->[]
-		| hd::ht -> if j=0 then peca@ht
+		| hd::ht -> if j=1 then peca@ht
 					else hd::(substituirPecaEmColuna ht (j-1) peca)
 ;;					
 let rec substituirPeca matriz i j peca =
 	match matriz with
 		| [] ->[]
-		| hd::ht -> if i = 0 then   (substituirPecaEmColuna hd j peca)::ht
+		| hd::ht -> if i = 1 then   (substituirPecaEmColuna hd j peca)::ht
 					else hd::(substituirPeca ht (i-1) j peca)
 ;;
 
-let moverPecaDeIJParaXY matriz i j x y = substituirPeca (substituirPeca matriz i j ["1"] ) x y ["B"] 
-;;
+let moverPecaDeIJParaXY matriz i j x y =  substituirPeca (substituirPeca matriz i j ["1"] ) x y ["B"] 
+	
 
+(*
+let moverPecaDeIJParaXY matriz i j x y = 
+	if (verificarPeca matriz i j) = 0 then substituirPeca (substituirPeca matriz i j ["1"] ) x y ["B"] 
+	else moverPeca matriz (escolhaDaPosicao 3) (escolhaDaPosicao 2) (escolhaDaPosicao 1) (escolhaDaPosicao 0)(*o primeiro eh na linha e depois coluna da linha que ele esta*)
+	
+;;*)
 let comerPecaDeIJParaXY  matriz i j x y k l = substituirPeca (substituirPeca (substituirPeca matriz k l ["1"]) i j ["1"]) x y ["B"]
-;;
-
-
-let moverPeca matriz i j x y = moverPecaDeIJParaXY matriz x y i j  
 ;;
 
 let escolhaDaPosicao numeroDeRequisicao = 
@@ -73,29 +75,89 @@ let moverPeca matriz i j x y =
 
 let turnoDoJogador matriz = moverPeca matriz (escolhaDaPosicao 3) (escolhaDaPosicao 2) (escolhaDaPosicao 1) (escolhaDaPosicao 0)(*o primeiro eh na linha e depois coluna da linha que ele esta*)
 ;;
+
+let rec verificarPecaNaLinhaDoComputadorBranca lista i j = 
+	match lista with
+		| []->[]
+		| hd::ht->	if hd="B" then i::j::[]
+					else verificarPecaNaLinhaDoComputadorBranca ht i (j+1)
+;;
+
+let rec verificarPecaNaLinhaDoComputadorPreta lista i j = 
+	match lista with
+		| []->[]
+		| hd::ht->	if hd="P" then i::j::[]
+					else verificarPecaNaLinhaDoComputadorPreta ht i (j+1)
+;;
+
+let rec verificarPecaMaisAFrenteERetornarOPosicaoIJ matriz listaAuxiliar i = 
+	match matriz with
+		| []-> []
+		| hd::ht-> if (verificarPecaNaLinhaDoComputadorBranca hd i 1)!=[] then
+						verificarPecaMaisAFrenteERetornarOPosicaoIJ ht hd (i+1)
+					else verificarPecaNaLinhaDoComputadorBranca listaAuxiliar i 1 
+;;
+let rec verificarPecaMaisAFrenteERetornarOPosicaoXY matriz listaAuxiliar i = 
+	match matriz with
+		| []-> []
+		| hd::ht-> if (verificarPecaNaLinhaDoComputadorPreta hd i 1)!=[] then
+						verificarPecaMaisAFrenteERetornarOPosicaoIJ ht hd (i+1)
+					else verificarPecaNaLinhaDoComputadorPreta listaAuxiliar i 1 
+;;
+let moverPecaDoComputador matriz = 
+;;
+
+let turnoDoComputador matriz  = 
+	let posicaoIJ  = verificarPecaMaisAFrenteERetornarOPosicaoIJ matriz
+	in let posicaoXY = verificarPecaMaisAFrenteERetornarOPosicaoXY matriz
+	in moverPecaDoComputador matriz 
+;;
 	
-let turnar matriz numeroDeTurno =  turnoDoJogador matriz
+let turnar matriz numeroDeTurno = if (numeroDeTurno mod 2) =0  then turnoDoJogador matriz
+									else turnoDoComputador matriz
 ;;	
 let rec printarMatriz matriz turno j matrizNova =
 	match matriz with
 		| [] -> print_string ""
-		| hd::ht -> if j =1 then(   print_string "\n\nJogo Atual :\n\n";
-									print_string"    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |";
-									print_string "\n    .   .   .   .   .   .   .   . ";
-									print_string "\n";
-									print_int j;
-									print_string " - ";	
-									(printarLista hd 1 j (appendLista matrizNova hd) turno); 
-									print_string "\n";
-									(printarMatriz ht turno (j+1) (appendLista matrizNova hd) )
-								)
-					else		    ( print_string "\n";
-									print_int j;
-									print_string " - ";	
-									(printarLista hd 1 j (appendLista matrizNova hd) turno); 
-									print_string "\n";
-									(printarMatriz ht turno (j+1) (appendLista matrizNova hd) 	 )
-								)
+		| hd::ht -> if (turno mod 2) =0	then
+		
+						if j =1  then(  print_string "\n\nTurno do Jogador :\n\n";
+										print_string "Jogo Atual :\n\n";
+										print_string"    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |";
+										print_string "\n    .   .   .   .   .   .   .   . ";
+										print_string "\n";
+										print_int j;
+										print_string " - ";	
+										(printarLista hd 1 j (appendLista matrizNova hd) turno); 
+										print_string "\n";
+										(printarMatriz ht turno (j+1) (appendLista matrizNova hd) )
+									)
+						else		    ( print_string "\n";
+										print_int j;
+										print_string " - ";	
+										(printarLista hd 1 j (appendLista matrizNova hd) turno); 
+										print_string "\n";
+										(printarMatriz ht turno (j+1) (appendLista matrizNova hd) 	 )
+									)
+					else 
+						if j =1  then(  print_string "\nTurno do Computador :\n\n";
+										print_string "Jogo Atual :\n\n";
+										print_string"    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |";
+										print_string "\n    .   .   .   .   .   .   .   . ";
+										print_string "\n";
+										print_int j;
+										print_string " - ";	
+										(printarLista hd 1 j (appendLista matrizNova hd) turno); 
+										print_string "\n";
+										(printarMatriz ht turno (j+1) (appendLista matrizNova hd) )
+									)
+						else		    ( print_string "\n";
+										print_int j;
+										print_string " - ";	
+										(printarLista hd 1 j (appendLista matrizNova hd) turno); 
+										print_string "\n";
+										(printarMatriz ht turno (j+1) (appendLista matrizNova hd) 	 )
+									)
 		
 		and	printarLista lista i j matriz turno  = 
 			match lista with
